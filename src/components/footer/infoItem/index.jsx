401fd8accf98089eefaceb2af4b7321d12878671
scziskh <muzychukserhii@gmail.com>
2022-05-29 10:07:34 +0300

    footer bug fixed

--- src/components/footer/infoItem/index.jsx
+++ src/components/footer/infoItem/index.jsx
@@ -9,7 +9,11 @@ const FooterInfoItem = () => (
     <ul className="noLS">
       <Logo />
       <li>
-        <a href="google.com" target="_blank">
+        <a
+          href="https://www.google.com/maps/place/%D0%98%D0%B7%D0%B3%D0%BE%D1%82%D0%BE%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5+%D0%B1%D1%80%D0%BE%D1%88%D1%8E%D1%80+%D0%B8+%D0%BF%D0%B5%D1%87%D0%B0%D1%82%D1%8C+%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3%D0%BE%D0%B2+%D0%B2+%D0%9A%D0%B8%D0%B5%D0%B2%D0%B5/@50.4522412,30.615739,15z/data=!4m2!3m1!1s0x0:0xdeae5cf83341aa01?sa=X&ved=2ahUKEwirkZvlkoT4AhWTRPEDHdCFBrEQ_BJ6BAg5EAM"
+          target="_blank"
+          rel="noreferrer"
+        >
           г. Киев, ул. Краковская 6А
         </a>
       </li>
