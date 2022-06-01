eaa4abe98ab84489bfdfc5bb273de4a6589556fd
scziskh <muzychukserhii@gmail.com>
2022-06-01 13:40:56 +0300

    functions

--- src/components/calculator/functions/getPrice.js
+++ src/components/calculator/functions/getPrice.js
@@ -1,10 +1,4 @@
 import CalculatorData from '../data';
-import getBindingSize from './getBindingSize';
-import getTrimmingCost from './getTrimmingCost';
-import getTotalSheets from './getTotalSheets';
-import getLaminationCost from './getLaminationCost';
-import getLaminationAdj from './getLaminationAdj';
-import getPrintingCost from './getPrintingCost';
 
 const getPrice = (
   bindingType,
@@ -153,5 +147,137 @@ const getPrice = (
   console.log(Math.ceil(totalPrice));
   return Math.ceil(totalPrice);
 };
+
+const getBindingSize = (thickness, bindingType) => {
+  let size = 1;
+  while (CalculatorData.bindingType[bindingType][size]) {
+    if (thickness > CalculatorData.bindingType[bindingType][size].thickness) {
+      size++;
+    } else {
+      return size;
+    }
+  }
+  return -1;
+};
+
+const getLaminationAdj = (laminationCover, laminationInner) => {
+  if (laminationCover || laminationInner) {
+    //Если есть ламинация и на обложке, и на внутреннем блоке
+    if (laminationCover && laminationInner) {
+      let adjustment;
+      laminationCover === laminationInner //Если она одинакова, то доп приладки не понадобится
+        ? (adjustment = CalculatorData.lamination[laminationInner].adjustment)
+        : (adjustment =
+            CalculatorData.lamination[laminationInner].adjustment +
+            CalculatorData.lamination[laminationCover].adjustment);
+      return adjustment;
+    }
+
+    //Если ламинация только обложки
+    if (laminationCover) {
+      return CalculatorData.lamination[laminationCover].adjustment;
+    }
+    //Если ламинация только внутреннего блока
+    return CalculatorData.lamination[laminationInner].adjustment;
+  }
+
+  //Если ламинации нет
+  return 0;
+};
+
+const getLaminationCost = (lamination, totalSheets) => {
+  if (lamination) {
+    let laminationCoef;
+    let k = totalSheets;
+
+    //Стоимость при тираже больше 500 листов не падает
+    if (k > 500) {
+      k = 500;
+    }
+    //Функция вычисления коефициента цены ламинации
+    laminationCoef = 1.8 * Math.pow(k, -0.2);
+
+    //Минимальный коефициент = 1
+    if (laminationCoef > 1) {
+      laminationCoef = 1;
+    }
+
+    //Стоимость ламинации
+    return (
+      totalSheets * laminationCoef * CalculatorData.lamination[lamination].cost
+    );
+  }
+  //Если нет ламинации
+  return 0;
+};
+
+const getPrintingCost = (printing, totalPages) => {
+  //Если есть страницы
+  if (totalPages) {
+    let printingCoef;
+    let k = totalPages;
+
+    //Стоимость при тираже больше 1000 не падает
+    if (k > 1000) {
+      k = 1000;
+    }
+
+    //Функции для цветной и ч/б печати
+    printing === 'oneSidedGrayscale' || printing === 'twoSidedGrayscale'
+      ? (printingCoef = 2.2 * Math.pow(k, -0.3))
+      : (printingCoef = 1.6 * Math.pow(k, -0.2));
+
+    //Минимальный коефициент = 1
+    if (printingCoef > 1) {
+      printingCoef = 1;
+    }
+
+    //Вычисление стоимости печати
+    return totalPages * CalculatorData.printing[printing].cost * printingCoef;
+  }
+  //Если нет печати
+  return 0;
+};
+
+const getTrimmingCost = printingCount => {
+  let trimmingCoef;
+  let trimmingCost;
+  let k = printingCount;
+
+  //Стоимость при тираже больше 100 изделий не падает
+  if (k > 100) {
+    k = 100;
+  }
+  //Функция вычисления коефициента цены подрезки
+  trimmingCoef = Math.pow(k, -0.1);
+
+  //Минимальный коефициент = 1
+  if (trimmingCoef > 1) {
+    trimmingCoef = 1;
+  }
+
+  //Стоимость подрезки не меньше минимальной
+  trimmingCost = printingCount * trimmingCoef * CalculatorData.trimming.cost;
+  if (trimmingCost < CalculatorData.trimming.minСost) {
+    return CalculatorData.trimming.minСost;
+  }
+  return trimmingCost;
+};
+
+const getTotalSheets = (printedSheets, printingCount, formatCoef) => {
+  //Возвращаем меньшее значение из двух возможных вариантов
+  if (
+    Math.ceil(printedSheets * formatCoef) * printingCount >
+    (printedSheets * formatCoef * Math.ceil(printingCount * formatCoef)) /
+      formatCoef
+  ) {
+    return (
+      (printedSheets * formatCoef * Math.ceil(printingCount * formatCoef)) /
+      formatCoef
+    );
+  }
+  return Math.ceil(printedSheets * formatCoef) * printingCount;
+};
+
 
 export default getPrice;
